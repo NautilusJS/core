b187a842783c6edcf2e3b5edbe9278c93b388941
mailmindlin <liamttlyrox@gmail.com>
2019-03-22 16:58:55 -0400

    Complete interface test checks

--- src/test/java/com/mindlin/jsast/impl/parser/InterfaceDeclarationTest.java
+++ src/test/java/com/mindlin/jsast/impl/parser/InterfaceDeclarationTest.java
@@ -3,11 +3,19 @@ package com.mindlin.jsast.impl.parser;
 import static com.mindlin.jsast.impl.parser.JSParserTest.*;
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
-import com.mindlin.jsast.tree.PropertyDeclarationTree;
+import com.mindlin.jsast.tree.MethodSignatureTree;
+import com.mindlin.jsast.tree.Modifiers;
+import com.mindlin.jsast.tree.PropertySignatureTree;
+import com.mindlin.jsast.tree.SignatureDeclarationTree.CallSignatureTree;
+import com.mindlin.jsast.tree.SignatureDeclarationTree.ConstructSignatureTree;
 import com.mindlin.jsast.tree.Tree.Kind;
 import com.mindlin.jsast.tree.type.InterfaceDeclarationTree;
+import com.mindlin.jsast.tree.type.SpecialTypeTree.SpecialType;
+import com.mindlin.jsast.tree.type.TypeElementTree;
 
 public class InterfaceDeclarationTest {
 	
@@ -21,49 +29,108 @@ public class InterfaceDeclarationTest {
 	public void testSingleMember() {
 		InterfaceDeclarationTree iface = parseStatement("interface HasName {name: string;}", Kind.INTERFACE_DECLARATION);
 		assertIdentifier("HasName", iface.getName());
+		assertEquals(0, iface.getHeritage().size());
 		
-		PropertyDeclarationTree prop0 = assertSingleElementKind(Kind.PROPERTY_DECLARATION, iface.getDeclaredMembers());
-		
-		fail("Not finished");
+		PropertySignatureTree prop0 = assertSingleElementKind(Kind.PROPERTY_SIGNATURE, iface.getDeclaredMembers());
+		assertModifiers(prop0.getModifiers(), Modifiers.NONE);
+		assertIdentifier("name", prop0.getName());
+		assertSpecialType(SpecialType.STRING, prop0.getType());
 	}
 	
 	@Test
 	public void testMultipleMembers() {
 		InterfaceDeclarationTree iface = parseStatement("interface TwoPartName {fName: string; lName: string;}", Kind.INTERFACE_DECLARATION);
 		assertIdentifier("TwoPartName", iface.getName());
+		assertEquals(0, iface.getHeritage().size());
+		List<? extends TypeElementTree> members = iface.getDeclaredMembers();
+		assertEquals(2, members.size());
 		
-		fail("Not finished");
+		
+		PropertySignatureTree prop0 = assertKind(Kind.PROPERTY_SIGNATURE, members.get(0));
+		assertModifiers(prop0.getModifiers(), Modifiers.NONE);
+		assertIdentifier("fName", prop0.getName());
+		assertSpecialType(SpecialType.STRING, prop0.getType());
+		
+		PropertySignatureTree prop1 = assertKind(Kind.PROPERTY_SIGNATURE, members.get(1));
+		assertModifiers(prop1.getModifiers(), Modifiers.NONE);
+		assertIdentifier("lName", prop1.getName());
+		assertSpecialType(SpecialType.STRING, prop1.getType());
 	}
 	
 	@Test
 	public void testSimpleMethodSignature() {
-		InterfaceDeclarationTree iface = parseStatement("interface HasFoo { foo(); }", Kind.INTERFACE_DECLARATION);
+		InterfaceDeclarationTree iface = parseStatement("interface HasMethod { toString(): string; }", Kind.INTERFACE_DECLARATION);
+		assertIdentifier("HasMethod", iface.getName());
+		assertEquals(0, iface.getHeritage().size());
+		List<? extends TypeElementTree> members = iface.getDeclaredMembers();
+		assertEquals(1, members.size());
 		
-		fail("Not finished");
+		//TODO: we need more method signature tests
+		
+		MethodSignatureTree prop0 = assertKind(Kind.METHOD_SIGNATURE, members.get(0));
+		assertModifiers(prop0.getModifiers(), Modifiers.NONE);
+		assertEquals(0, prop0.getTypeParameters().size());
+		assertIdentifier("toString", prop0.getName());
+		assertEquals(0, prop0.getParameters().size());
+		assertSpecialType(SpecialType.STRING, prop0.getReturnType());
 	}
 	
 	@Test
 	public void testConstructorSignature() {
-		InterfaceDeclarationTree iface = parseStatement("interface HasCtor { new (); }", Kind.INTERFACE_DECLARATION);
+		InterfaceDeclarationTree iface = parseStatement("interface HasCtor { new (): string; }", Kind.INTERFACE_DECLARATION);
+		assertIdentifier("HasCtor", iface.getName());
+		assertEquals(0, iface.getHeritage().size());
+		List<? extends TypeElementTree> members = iface.getDeclaredMembers();
+		assertEquals(1, members.size());
 		
-		fail("Not finished");
+		ConstructSignatureTree prop0 = assertKind(Kind.CONSTRUCT_SIGNATURE, members.get(0));
+		assertEquals(0, prop0.getTypeParameters().size());
+		assertNull(prop0.getName());
+		assertEquals(0, prop0.getParameters().size());
+		assertSpecialType(SpecialType.STRING, prop0.getReturnType());
+	}
+	
+	@Test
+	public void testCallSignature() {
+		InterfaceDeclarationTree iface = parseStatement("interface HasCallSignature { (): void; }", Kind.INTERFACE_DECLARATION);
+		assertIdentifier("HasCallSignature", iface.getName());
+		assertEquals(0, iface.getHeritage().size());
+		List<? extends TypeElementTree> members = iface.getDeclaredMembers();
+		assertEquals(1, members.size());
+		
+		
+		CallSignatureTree prop0 = assertKind(Kind.CALL_SIGNATURE, members.get(0));
+		assertEquals(0, prop0.getTypeParameters().size());
+		assertNull(prop0.getName());
+		assertEquals(0, prop0.getParameters().size());
+		assertSpecialType(SpecialType.VOID, prop0.getReturnType());
 	}
 	
 	@Test
 	public void testReadonlyProperty() {
 		InterfaceDeclarationTree iface = parseStatement("interface Foo { readonly bar: string; }", Kind.INTERFACE_DECLARATION);
+		assertIdentifier("Foo", iface.getName());
+		assertEquals(0, iface.getHeritage().size());
+		List<? extends TypeElementTree> members = iface.getDeclaredMembers();
+		assertEquals(1, members.size());
 		
-		fail("Not finished");
+
+		PropertySignatureTree prop0 = assertKind(Kind.PROPERTY_SIGNATURE, members.get(0));
+		assertModifiers(prop0.getModifiers(), Modifiers.READONLY);
+		assertIdentifier("bar", prop0.getName());
+		assertSpecialType(SpecialType.STRING, prop0.getType());
 	}
 	
 	// Some extra punctuation checks
 	@Test
 	public void testCommaSeparated() {
-		
+		//TODO: finish
 	}
 	
 	@Test
 	public void testNoEndPunctuation() {
-		
+		//TODO: finish
 	}
+	
+	//TODO: Heritage clauses
 }
