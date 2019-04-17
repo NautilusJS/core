4a0fe145d4015140fff7c78f4ed2855cde74db17
mailmindlin <liamttlyrox@gmail.com>
2019-04-17 18:38:33 -0400

    JavaDoc

--- src/main/java/com/mindlin/jsast/type/ObjectType.java
+++ src/main/java/com/mindlin/jsast/type/ObjectType.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 public interface ObjectType extends Type, GenericType {
 	/**
 	 * Get the 'this' type parameter
-	 * @return
+	 * @return 'this' type parameter
 	 */
 	TypeParameter thisType();
 	
