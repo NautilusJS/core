94e20bbbafba9eee21060745735fdb94626757e1
mailmindlin <liamttlyrox@gmail.com>
2019-04-17 18:38:06 -0400

    Fix type

--- src/main/java/com/mindlin/jsast/writer/SourceMap.java
+++ src/main/java/com/mindlin/jsast/writer/SourceMap.java
@@ -2,15 +2,15 @@ package com.mindlin.jsast.writer;
 
 import java.util.Map;
 
-import com.mindlin.jsast.fs.FilePosition;
 import com.mindlin.jsast.fs.SourceFile;
+import com.mindlin.jsast.fs.SourcePosition;
 
 public class SourceMap {
 	Map<String, SourceFile> sources;
 	
 	public void addMapping(String sourceName, long srcPos, int dstLine, int dstCol, String name) {
 		SourceFile source = sources.get(sourceName);
-		FilePosition src = source.getOffsetPosotion(srcPos);
+		SourcePosition src = source.getOffsetPosotion(srcPos);
 	}
 	
 }
