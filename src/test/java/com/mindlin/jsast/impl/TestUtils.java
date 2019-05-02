54b0566747a394471548107632429c0ca5696e03
mailmindlin <liamttlyrox@gmail.com>
2019-05-02 15:48:50 -0400

    Fix TestUtils

--- src/test/java/com/mindlin/jsast/impl/TestUtils.java
+++ src/test/java/com/mindlin/jsast/impl/TestUtils.java
@@ -1,25 +1,34 @@
 package com.mindlin.jsast.impl;
 
-public class TestUtils {
-	public static final void assertNumberEquals(Number expected, Number actual, String message) {
-		if (expected.longValue() != actual.longValue() && Math.abs(expected.doubleValue() - actual.doubleValue()) > .0001)
-			throw new AssertionError(message);
-	}
-	public static final void assertNumberEquals(Number expected, Number actual) {
+import java.util.Arrays;
 
-		if (expected.longValue() != actual.longValue() && Math.abs(expected.doubleValue() - actual.doubleValue()) > .0001) {
-			AssertionError e = new AssertionError("expected:<"+expected+"> but was:<"+actual+">");
+import org.junit.Assume;
+import org.junit.jupiter.api.Assertions;
+
+import junit.framework.ComparisonFailure;
+
+public class TestUtils {
+	public static final double EPSILON = .0001;
+	
+	public static final void assertNumberEquals(Number expected, Number actual, String message) {
+		Assume.assumeNotNull(expected);
+		Assertions.assertNotNull(actual, message);
+		
+		if (expected.longValue() != actual.longValue() && Math.abs(expected.doubleValue() - actual.doubleValue()) > EPSILON)
+			throw new ComparisonFailure(message, expected.toString(), actual.toString());
+	}
+	
+	public static final void assertNumberEquals(Number expected, Number actual) {
+		if (expected.longValue() != actual.longValue() && Math.abs(expected.doubleValue() - actual.doubleValue()) > EPSILON) {
+			ComparisonFailure e = new ComparisonFailure(null, expected.toString(), actual.toString());
 			StackTraceElement[] trace = getStackTraceRelative(1);
 			e.setStackTrace(trace);
 			throw e;
 		}
 	}
+	
 	static final StackTraceElement[] getStackTraceRelative(int rel) {
 		StackTraceElement[] trace = Thread.currentThread().getStackTrace();
-		final int offset = rel + 2;
-		StackTraceElement[] result = new StackTraceElement[trace.length - offset];
-		for (int i = 0, l = result.length; i < l; i++)
-			result[i] = trace[i + offset];
-		return result;
+		return Arrays.copyOf(trace, trace.length - rel - 2);
 	}
 }
