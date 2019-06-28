f6170732b78ea9e68126355ebd21384030f9cb42
mailmindlin <liamttlyrox@gmail.com>
2019-06-28 12:52:04 -0400

    Add more constructors for NotFinishedException

--- src/main/java/com/mindlin/jsast/exception/NotFinishedException.java
+++ src/main/java/com/mindlin/jsast/exception/NotFinishedException.java
@@ -1,5 +1,21 @@
 package com.mindlin.jsast.exception;
 
 public class NotFinishedException extends RuntimeException {
+
+	public NotFinishedException() {
+		super();
+	}
+
+	public NotFinishedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public NotFinishedException(String message) {
+		super(message);
+	}
+
+	public NotFinishedException(Throwable cause) {
+		super(cause);
+	}
 	
 }
